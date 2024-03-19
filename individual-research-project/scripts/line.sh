2fa00a43c0fce4c1f1b34ae0b1d9313d3a64005c
yumu987 <warmaradio@163.com>
2024-03-19 18:06:14 +0000

    Update

---
+++ individual-research-project/scripts/line.sh
@@ -0,0 +1,11 @@
+# chmod +x line.sh
+# ./line.sh
+
+# Display contributions
+git log --author="yumu987" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -
+
+# Display total contributions
+# git log --format='%aN' | sort -u | while read name; do echo -en "$name\t"; git log --author="$name" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s\n", add, subs, loc }' -; done
+
+# Exit the script
+exit
