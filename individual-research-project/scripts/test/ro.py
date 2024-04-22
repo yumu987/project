d40a19a4931d88d6415d86431bc59cee471aea24
yumu987 <warmaradio@163.com>
2024-04-22 09:25:06 +0100

    Update

---
+++ individual-research-project/scripts/test/ro.py
@@ -0,0 +1,19 @@
+x = int(3.5)
+print(f"x:{x}")
+y = int(round(3.5))
+print(f"y:{y}")
+z = int(3.1)
+print(f"z:{z}")
+i = int(round(3.1))
+print(f"i:{i}")
+j = int(3.9)
+print(f"j:{j}")
+r = int(round(3.9))
+print(f"r:{r}")
+
+# x:3
+# y:4
+# z:3
+# i:3
+# j:3
+# r:4
